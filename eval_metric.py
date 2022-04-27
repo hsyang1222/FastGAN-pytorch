ae61df3b2cf68c11408f8d50f9f8c4966e0d7efc
nrhan0623 <nrhan0623@gmail.com>
2022-04-27 21:53:10 +0900

    wandb에 metric 결과 upload 되도록 개선

--- eval_metric.py
+++ eval_metric.py
@@ -5,6 +5,7 @@ import torchvision
 from torchvision import transforms
 from tqdm import tqdm
 import lpips
+import wandb
 
 
 def get_data_loader(image_path, image_size=1024, batch_size=8):
@@ -31,6 +32,7 @@ def calculate_lpip_sum_given_images(loss_fn, group_of_images, device):
     lpips_sum = loss_fn(group_of_images[0:num_rand_outputs - 1], group_of_images[1:num_rand_outputs])
     return float(lpips_sum.sum())
 
+
 def calculate_lpips(group_of_images, device):
     lpips_sum = 0
     loss_fn = lpips.LPIPS(net='alex').to(device)
@@ -45,23 +47,38 @@ def calculate_lpips(group_of_images, device):
         lpips_sum += calculate_lpip_sum_given_images(loss_fn, group_of_images[start_index:end_index], device)
     return lpips_sum / (len(group_of_images)-1)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='evaluation metric')
     parser.add_argument('--real_image_path', type=str)
     parser.add_argument('--fake_image_path', type=str)
+    parser.add_argument('--start_iter', type=int, default=3)
+    parser.add_argument('--end_iter', type=int, default=5)
     args = parser.parse_args()
 
-    real_image_data_loader = get_data_loader(args.real_image_path)
-    fake_image_data_loader = get_data_loader(args.fake_image_path)
-    fake_image_tensor = get_data_tensor_from_data_loader(fake_image_data_loader)
-    device = torch.device("cuda:0")
-    lpips = calculate_lpips(fake_image_tensor, device)
-    score_model = generative_model_score.GenerativeModelScore(real_image_data_loader)
-    score_model.lazy_mode(True)
-    score_model.load_or_gen_realimage_info(device)
-    score_model.model_to(device)
-    score_model.lazy_forward(real_forward=False, fake_image_tensor=fake_image_tensor, device=device)
-    score_model.calculate_fake_image_statistics()
-    metrics = score_model.calculate_generative_score()
-    metrics['lpips'] = lpips
-    print(metrics)
+    wandb.login()
+    wandb_name = args.fake_image_path.split('/')[-1]
+    wandb.init(project="LightweightGAN", config=args, name=wandb_name)
+    try:
+        for each_iter in range(args.start_iter*10000, (args.end_iter+1)*10000, 10000):
+            each_fake_image_path = args.fake_image_path + '/eval_' + str(each_iter)
+            real_image_data_loader = get_data_loader(args.real_image_path)
+            fake_image_data_loader = get_data_loader(each_fake_image_path)
+            fake_image_tensor = get_data_tensor_from_data_loader(fake_image_data_loader)
+            device = torch.device("cuda:0")
+            lpips_score = calculate_lpips(fake_image_tensor, device)
+            score_model = generative_model_score.GenerativeModelScore(real_image_data_loader)
+            score_model.lazy_mode(True)
+            score_model.load_or_gen_realimage_info(device)
+            score_model.model_to(device)
+            score_model.lazy_forward(real_forward=False, fake_image_tensor=fake_image_tensor, device=device)
+            score_model.calculate_fake_image_statistics()
+            metrics = score_model.calculate_generative_score()
+            metrics['lpips'] = lpips_score
+            print(metrics)
+
+            wandb.log(metrics, step=each_iter)
+    except Exception as exception:
+        print(str(exception))
+    finally:
+        wandb.finish()
